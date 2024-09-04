fe041f36f988a4de52e794bc48511fc787473d1c
RadouaneAbn <radouaneabn@gmail.com>
2024-09-04 02:59:43 +0100

    Task 3

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import AppContorller from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const route = Router();
 
 route.get('/status', AppContorller.getStatus);
 
 route.get('/stats', AppContorller.getStats);
+
+route.post('/users', UsersController.postNew);
 
 module.exports = route;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import route from './routes/index';
 const port = process.env.PORT || 5000;
 
 const app = express();
+app.use(express.json());
 app.use(route);
 
 app.listen(port, () => {
